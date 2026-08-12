510588265a73bb49818a55035890eebe50d13363
neoride-pipeline <pipeline@neoride.ru>
2026-08-12 13:51:34 +0300

    авто-обновление каталога

--- data/catalog.js
+++ data/catalog.js
@@ -90,10 +90,9 @@ const CATALOG = [
   "brand": "Kugoo",
   "cat": "самокат",
   "price": 23900,
-  "stock": "no",
+  "stock": "wait",
   "src": [
-   "k2025",
-   "koptom"
+   "k2025"
   ],
   "pop": 220,
   "hit": false,
